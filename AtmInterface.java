abebdda48e81e73a2177888c90e870027ac2e31d
Studentdnyaneshwar <158763290+Studentdnyaneshwar@users.noreply.github.com>
2024-02-08 13:44:17 +0530

    Delete AtmInterface.java

--- AtmInterface.java
+++
@@ -1,8 +0,0 @@
-package miniAtmInterface;
-
-public interface AtmInterface {
-	public void viewBalance(); 
-	public void withdrawAmount(double withdrawAmount); 
-	public void depositBalance(double depositAmount); 
-	public void viewMiniStatement(); 
-}
